1d81f67e11b38083fcec7813034948dd67804759
揭志胜 <15870608093@163.com>
2022-03-19 15:21:44 +0800

    build: 删除dist、node_modules目录

--- dist/hooks/index.d.ts
+++
@@ -1,2 +0,0 @@
-import useComponentCommon from "./useComponentCommon";
-export { useComponentCommon, };
--- dist/test.plugin.d.ts
+++
@@ -1,5 +0,0 @@
-import { App } from 'vue';
-declare const plugin: {
-    install: (app: App) => void;
-};
-export default plugin;
